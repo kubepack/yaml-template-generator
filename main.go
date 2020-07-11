61a3e0db1cf3891ea65db63d55f6fbe548df9a23
Tamal Saha <tamal@appscode.com>
2020-07-11 09:49:56 -0700

    added-all

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+type SA struct {
+	A string
+	B string
+}
 func main() {
-
+	m := map[SA]string{}
+	data, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
 }
