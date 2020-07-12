9ff5c57880ba9f92ee1cc1d664b81c1b90e3be23
Tamal Saha <tamal@appscode.com>
2020-07-11 20:03:46 -0700

    added-all

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/tamalsaha/json-demo
 
 go 1.14
+
+require (
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
+	sigs.k8s.io/yaml v1.2.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"gopkg.in/yaml.v3"
+	y2 "sigs.k8s.io/yaml"
 )
+
+var yaml_text = `services:
+  # Node.js gives OS info about the node (Host)
+  nodeinfo: &function
+    image: functions/nodeinfo:latest
+    labels:
+      function: "true"
+    depends_on:
+      - gateway
+    networks:
+      - functions
+    environment:
+      no_proxy: "gateway"
+      https_proxy: $https_proxy
+    deploy:
+      placement:
+        constraints:
+          - 'node.platform.os == linux'
+  # Uses cat to echo back response, fastest function to execute.
+  echoit:
+    <<: *function
+    image: functions/alpine:health
+    environment:
+      fprocess: "cat"
+      no_proxy: "gateway"
+      https_proxy: $https_proxy`
 
 type SA struct {
 	A string `json:"a"`
@@ -11,10 +38,27 @@ type SA struct {
 }
 
 func main() {
-	m := map[string]SA{}
-	data, err := json.MarshalIndent(m, "", "  ")
+	var mj map[string]interface{}
+	err := yaml.Unmarshal([]byte(yaml_text), &mj)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(data))
+	str, err := yaml.Marshal(mj)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(str))
+
+	str, err = y2.YAMLToJSON([]byte(str))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(str))
+
+	//m := map[string]SA{}
+	//data, err := json.MarshalIndent(m, "", "  ")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(string(data))
 }
